93e79406a9872e374b83b16833fa7de8ac3c6511
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2022-12-13 10:10:08 +0100

    Exercise6.py

---
+++ Practice_Python/Exercise6.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+
+Ask the user for a string and print out whether this string is a palindrome or not.
+(A palindrome is a string that reads the same forwards and backwards.)
+"""
+
+import os
+clear = lambda: os.system("cls")
+clear()
+
+
+text = input("I will check if your word is a palindrome or not.\nEnter a single word: ")
